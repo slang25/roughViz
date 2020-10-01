8598f8a5f0dc786f73f5c1bc7fc0dd8c6efa16a3
Ken Snyder <ken@ken.net>
2020-09-30 20:49:49 -0700

    chore: adjustments to build/bundler chore: rebuilt to ensure d3.array error is gone

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,38 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
+import terser from "rollup-plugin-terser";
 import pkg from "./package.json";
 import { builtinModules } from "module";
 import analyze from "rollup-plugin-analyzer";
 
+// makes all non-core deps external; allowing consuming app to gain better reuse
 const external = [
-  // ...(pkg.dependencies ? Object.keys(pkg.dependencies) : []),
   ...(pkg.peerDependencies ? Object.keys(pkg.peerDependencies) : []),
   ...(pkg.optionalDependencies ? Object.keys(pkg.optionalDependencies) : []),
   ...builtinModules,
 ];
 
+// required for IIFE module export
+const globals = { d3: "d3", roughjs: "roughjs" };
+
+// Rollup configuration for the passed in module system
 const moduleConfig = (moduleSystem) => ({
   input: "src/index.js",
   output: {
+    ...(moduleSystem === "iife" || moduleSystem === "umd"
+      ? { name: pkg.name.replace(/\-/g, ""), globals }
+      : {}),
     dir: `dist/${moduleSystem}`,
     format: moduleSystem,
     sourcemap: false,
   },
   external,
-  plugins: [resolve(), ...(moduleSystem === "es" ? [analyze()] : [])],
+  plugins: [
+    ...(moduleSystem === "iife" ? { terser } : {}),
+    commonjs(),
+    resolve(),
+    ...(moduleSystem === "es" && process.env.ANALYZE ? [analyze()] : []),
+  ],
 });
 
-export default [moduleConfig("cjs"), moduleConfig("es")];
+export default [moduleConfig("cjs"), moduleConfig("iife"), moduleConfig("es")];
--- src/utils/useFont.js
+++ src/utils/useFont.js
@@ -1,7 +1,5 @@
-const familyLoader = (family = `https://fonts.googleapis.com/css2?family=${family.replace(
-  / /g,
-  "+"
-)}&display=swap`);
+const familyLoader = (family) =>
+  `https://fonts.googleapis.com/css2?family=${family.replace(/ /g, "+")}&display=swap`;
 
 export let preloadedFonts = [];
 export let requestedFonts = [];
